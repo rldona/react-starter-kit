0ef54390ad8829aa282f1e12c1f9e6db9fa24ed0
Raúl López Doña <rldona@gmail.com>
2021-06-12 14:17:44 +0200

    [FIX] Modified the module moduleNameMapper and added an additional test

--- __tests__/App.test.js
+++ __tests__/App.test.js
@@ -2,10 +2,16 @@ import { render, screen } from '@testing-library/react';
 
 import App from '../src/components/App';
 
+test('<App /> rendered', () => {
+  const app = render(<App />);
+
+  expect(app).toBeDefined();
+});
+
 test('Renders "react starter kit" title', () => {
   render(<App />);
 
-  const title = screen.getByText(/react starter kit/i);
+  const title = screen.queryByText(/react starter kit/i);
 
   expect(title).toBeDefined();
 });
--- jest.config.ts
+++ jest.config.ts
@@ -71,7 +71,7 @@ export default {
 
   // A map from regular expressions to module names or to arrays of module names that allow to stub out resources with a single module
   moduleNameMapper: {
-    '\\.(css|scss|jpg|png|gif)$': '<rootDir>/src/components/App/index.tsx',
+    '\\.(css|scss|jpg|png|gif)$': ['<rootDir>/src/components/App/index.tsx'],
   },
 
   // An array of regexp pattern strings, matched against all module paths before considered 'visible' to the module loader
