58cf4645b35578fea8b08df822ecdf03be9c7d12
Raúl López Doña <rldona@gmail.com>
2021-06-21 19:57:35 +0200

    [FEAT] Config storybook 2

--- src/components/StarterLogo/StarterLogo.stories.tsx
+++ src/components/StarterLogo/StarterLogo.stories.tsx
@@ -2,4 +2,5 @@ import {storiesOf} from '@storybook/react';
 
 import {StarterLogo} from '.';
 
+storiesOf('StarterLogo', module).add('with props', () => <StarterLogo />);
 storiesOf('StarterLogo', module).add('without props', () => <StarterLogo />);
