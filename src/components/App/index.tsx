6084e4f0710bbfbebc2564b495d65b5858d0e977
Raúl López Doña <rldona@gmail.com>
2021-10-07 15:52:49 +0200

    [Prettier] Fixed

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,6 +1,7 @@
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 
 import {StarterLogo} from '../StarterLogo';
+
 import './App.scss';
 
 export default function App(): JSX.Element {
