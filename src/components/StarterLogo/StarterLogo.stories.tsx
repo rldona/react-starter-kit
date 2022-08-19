8d582f2b5eb19ff330f09f545078e22fa3cfbbc7
Raúl López Doña <rldona@gmail.com>
2022-08-19 11:51:26 +0200

    feature-a

--- src/components/StarterLogo/StarterLogo.stories.tsx
+++ src/components/StarterLogo/StarterLogo.stories.tsx
@@ -2,4 +2,4 @@ import {storiesOf} from '@storybook/react';
 
 import {StarterLogo} from '.';
 
-storiesOf('StarterLogo', module).add('without props 2', () => <StarterLogo />);
+storiesOf('StarterLogo', module).add('Without props', () => <StarterLogo />);
