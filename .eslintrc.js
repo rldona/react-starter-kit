534a38ec30b54d8fc03f3b67e752eea20ecdc80e
Raúl López Doña <raul.lopez@cepsa.com>
2022-08-19 09:49:40 +0200

    fix: Resolve merge conflict

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,44 +1,34 @@
 module.exports = {
   parser: '@typescript-eslint/parser',
-  extends: [
-    'plugin:react/recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:prettier/recommended',
-  ],
+  extends: ['plugin:react/recommended', 'plugin:@typescript-eslint/recommended', 'plugin:prettier/recommended', 'plugin:storybook/recommended'],
   parserOptions: {
     ecmaVersion: 2018,
-    sourceType: 'module',
+    sourceType: 'module'
   },
   plugins: ['prettier'],
   rules: {
-    'prettier/prettier': [
-      'error',
-      {
-        singleQuote: true,
-        trailingComma: 'all',
-      },
-    ],
-    'react/prop-types': [
-      1,
-      {
-        ignore: ['context', 'tracking'],
-      },
-    ],
+    'prettier/prettier': ['error', {
+      singleQuote: true,
+      trailingComma: 'all'
+    }],
+    'react/prop-types': [1, {
+      ignore: ['context', 'tracking']
+    }],
     'react/react-in-jsx-scope': 'off',
-    'react/jsx-filename-extension': [1, {extensions: ['.tsx', '.ts', '.js']}],
-    '@typescript-eslint/no-var-requires': 0,
+    'react/jsx-filename-extension': [1, {
+      extensions: ['.tsx', '.ts', '.js']
+    }],
+    '@typescript-eslint/no-var-requires': 0
   },
   settings: {
     react: {
-      version: 'detect',
-    },
+      version: 'detect'
+    }
   },
-  overrides: [
-    {
-      files: ['*.ts', '*.tsx'],
-      rules: {
-        'react/prop-types': 'off',
-      },
-    },
-  ],
+  overrides: [{
+    files: ['*.ts', '*.tsx'],
+    rules: {
+      'react/prop-types': 'off'
+    }
+  }]
 };
